78f01a0b77d351410f56c57cfb1babaf7d342585
pprzemek56 <przemkopis@wp.pl>
2022-11-18 00:45:01 +0100

    implement add_array

--- multiplication.py
+++ multiplication.py
@@ -21,43 +21,51 @@ def print_multiplication(a, b):
     final_x = max(len(a_list), len(b_list))
     final_y = 2 + len(a_list)
 
-    result = [[-1 for _ in range(final_x)] for _ in range(final_y)]
+    multiplication_array = [[-1 for _ in range(final_x)] for _ in range(final_y)]
+    add_array = [[-1 for _ in range(len(a_list) + len(b_list))] for _ in range(len(a_list))]
 
     x = final_x - 1
     y = final_y - 1
 
     counter = 0
-
     while y >= 0:
         while x >= 0:
             if counter == 0 and x < len(a_list):
                 if len(a_list) < len(b_list):
-                    result[y][x + len_dif] = a_list[x]
+                    multiplication_array[y][x + len_dif] = a_list[x]
                 else:
-                    result[y][x] = a_list[x]
+                    multiplication_array[y][x] = a_list[x]
             elif counter == 1 and x < len(b_list):
                 if len(b_list) < len(a_list):
-                    result[y][x + len_dif] = b_list[x]
+                    multiplication_array[y][x + len_dif] = b_list[x]
                 else:
-                    result[y][x] = b_list[x]
+                    multiplication_array[y][x] = b_list[x]
             x -= 1
         if counter == 2:
             a_list.reverse()
             b_list.reverse()
             y = 0
+            second_counter = 0
             for i in range(len(a_list)):
                 for j in range(len(b_list)):
                     mul = int(a_list[i]) * int(b_list[j])
-                    if result[final_y - 3 - i][final_x - 1 - j] != -1:
-                        mul += result[final_y - 3 - i][final_x - 1 - j]
-                    if mul > 9 and result[final_y - 2][final_x - 2 - j] != -1:
-                        result[final_y - 3 - i][final_x - 2 - j] = int(mul / 10)
+                    if multiplication_array[final_y - 3 - i][final_x - 1 - j] != -1:
+                        mul += multiplication_array[final_y - 3 - i][final_x - 1 - j]
+                    if mul > 9 and multiplication_array[final_y - 2][final_x - 2 - j] != -1:
+                        multiplication_array[final_y - 3 - i][final_x - 2 - j] = int(mul / 10)
+                result = int(b) * int(a_list[i])
+                third_counter = len(add_array[0]) - 1 - second_counter
+                while result > 0:
+                    add_array[second_counter][third_counter] = result % 10
+                    result = int(result / 10)
+                    third_counter -= 1
+                second_counter += 1
         counter += 1
         x = final_x - 1
         y -= 1
 
-    print(result)
-
+    print(multiplication_array)
+    print(add_array)
 
 
 
