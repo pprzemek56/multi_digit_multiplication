500cd840a2d8fa340e624dca90b65a0efc329d97
pprzemek56 <przemkopis@wp.pl>
2022-12-19 19:46:06 +0100

    fix program

--- multiplication.py
+++ multiplication.py
@@ -17,7 +17,6 @@ def print_multiplication(a, b):
     a_list = [_ for _ in a]
     b_list = [_ for _ in b]
 
-    len_dif = abs(len(a_list) - len(b_list))
     final_x = 1 + max(len(a_list), len(b_list))
     final_y = 2 + len(a_list)
 
@@ -26,20 +25,17 @@ def print_multiplication(a, b):
 
     x = final_x - 1
     y = final_y - 1
-
+    a_len = len(a_list) - 1
+    b_len = len(b_list) - 1
     counter = 0
     while y >= 0:
         while x >= 0:
-            if counter == 0 and x < len(a_list):
-                if len(a_list) < len(b_list):
-                    multiplication_array[y][x + len_dif] = a_list[x]
-                else:
-                    multiplication_array[y][x] = a_list[x]
-            elif counter == 1 and x < len(b_list):
-                if len(b_list) < len(a_list):
-                    multiplication_array[y][x + len_dif] = b_list[x]
-                else:
-                    multiplication_array[y][x] = b_list[x]
+            if counter == 0 and a_len > -1:
+                multiplication_array[y][x] = a_list[a_len]
+                a_len -= 1
+            elif counter == 1 and b_len > -1:
+                multiplication_array[y][x] = b_list[b_len]
+                b_len -= 1
             x -= 1
         if counter == 2:
             a_list.reverse()
@@ -51,7 +47,7 @@ def print_multiplication(a, b):
                     mul = int(a_list[i]) * int(b_list[j])
                     if multiplication_array[final_y - 3 - i][final_x - 1 - j] != -1:
                         mul += multiplication_array[final_y - 3 - i][final_x - 1 - j]
-                    if mul > 9 and multiplication_array[final_y - 2][final_x - 2 - j] != -1:
+                    if mul > 9:
                         multiplication_array[final_y - 3 - i][final_x - 2 - j] = int(mul / 10)
                 result = int(b) * int(a_list[i])
                 third_counter = len(add_array[0]) - 1 - second_counter
@@ -64,40 +60,36 @@ def print_multiplication(a, b):
         x = final_x - 1
         y -= 1
 
-    for i in range(len(multiplication_array)):
-        for j in range(len(multiplication_array[i])):
-            print(multiplication_array[i][j], end=" ")
-        print()
 
-    # for i in range(len(multiplication_array)):
-    #     for j in range(len(multiplication_array[0])):
-    #         if j == 0:
-    #             if i == len(multiplication_array) - 1:
-    #                 print("*", end=" " * len(add_array[0]))
-    #             else:
-    #                 print(" ", end=" " * len(add_array[0]))
-    #         if multiplication_array[i][j] != -1:
-    #             print(multiplication_array[i][j], end="")
-    #         else:
-    #             print(" ", end="")
-    #     print()
-    # print("---" * len(multiplication_array[0]))
-    #
-    # for i in range(len(add_array)):
-    #     for j in range(len(add_array[0])):
-    #         if j == 0:
-    #             if i == len(add_array) - 1:
-    #                 print("+", end=" " * len(multiplication_array[0]))
-    #             else:
-    #                 print(" ", end=" " * len(multiplication_array[0]))
-    #         if add_array[i][j] != -1:
-    #             print(add_array[i][j], end="")
-    #         else:
-    #             print(" ", end="")
-    #     print()
-    # print("---" * len(multiplication_array[0]))
-    #
-    # print(" " * (len(multiplication_array[0]) + 1), main_product)
+    for i in range(len(multiplication_array)):
+        for j in range(len(multiplication_array[0])):
+            if j == 0:
+                if i == len(multiplication_array) - 1:
+                    print("*", end=" " * len(add_array[0]))
+                else:
+                    print(" ", end=" " * len(add_array[0]))
+            if multiplication_array[i][j] != -1:
+                print(multiplication_array[i][j], end="")
+            else:
+                print(" ", end="")
+        print()
+    print("---" * len(multiplication_array[0]))
+
+    for i in range(len(add_array)):
+        for j in range(len(add_array[0])):
+            if j == 0:
+                if i == len(add_array) - 1:
+                    print("+", end=" " * len(multiplication_array[0]))
+                else:
+                    print(" ", end=" " * len(multiplication_array[0]))
+            if add_array[i][j] != -1:
+                print(add_array[i][j], end="")
+            else:
+                print(" ", end="")
+        print()
+    print("---" * len(multiplication_array[0]))
+
+    print(" " * (len(multiplication_array[0])), main_product)
 
 
 if __name__ == "__main__":
